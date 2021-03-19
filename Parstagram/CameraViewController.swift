c0615f99fde6d35e81209009183f6d999902865c
ajss25 <alexshinjs@gmail.com>
2021-03-19 04:22:07 -0700

    Add query to Parse and load data from query

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -59,7 +59,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         // resize
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
+        let scaledImage = image.af.imageScaled(to: size)
         
         imageView.image = scaledImage
         
